7dd8b834399aad612e7829a34ea5fad0215ff790
sophie-jhkim <creatorsophie@gmail.com>
2022-11-06 23:32:22 +0900

    next.js api test

--- libs/client.ts
+++ libs/client.ts
@@ -1,5 +1,2 @@
 import { PrismaClient } from "@prisma/client";
-const client = new PrismaClient();
-client.user.create({
-    data: { email: "hi", name: "hi" },
-});
+export default new PrismaClient();
