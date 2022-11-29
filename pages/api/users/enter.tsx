8568e75bd16b14a20d4070c021e705a40a0cd0c1
sophie-jhkim <creatorsophie@gmail.com>
2022-11-29 23:37:01 +0900

    update create account

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,6 +6,34 @@ async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const {phone, email} = req.body;
+    const payload = phone? {phone: +phone} : {email}
+    const user = await client.user.upsert({
+        where:{
+            ...payload
+        },
+        create:{
+            name: "Anonymous",
+            ...payload
+        },
+        update:{},
+    })
+    console.log(user)
+/*   if(email){
+        user = await client.user.findUnique({
+            where : {
+                email
+            }
+        });
+        if(!user){
+            user = await client.user.create({
+                data:{
+                    name: "Anonymous",
+                    email,
+                }
+            })
+        }
+    } */
     console.log(req.body);
     res.json({ok: true});
 }
