7580c29efa3d020ae9e9404a9d5c09c3fa8e7765
sophie-jhkim <creatorsophie@gmail.com>
2023-02-01 00:27:48 +0900

    Feat: useUser 훅 보완

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import useSWR from "swr";
 /* swr은 old value와 new value를 비교해서 변화가 없다면 old value를 cache로 유지한다 
 사용자가 내 웹사이트의 다른 메뉴들을 왔다갔다 했을때도 비교하지만 다른 탭에서 다른짓(?)을 하다 왔을때도 바로 check(update)해주는 기특한녀석
@@ -9,9 +8,11 @@ import useSWR from "swr";
 const fetcher = (url:string) => fetch(url).then((response) => response.json());
 
 export default function useUser(){
-    const {data, error} = useSWR("/api/users/me", fetcher)
+    // const {data, error} = useSWR("/api/users/me", fetcher)
+    const {data, error} = useSWR("/api/users/me")
     const [ user, setUser ] = useState();
     const router = useRouter();
+    /*
     useEffect(()=>{
         fetch("/api/users/me")
         .then(response => response.json())
@@ -22,5 +23,13 @@ export default function useUser(){
             setUser(data.profile);
         })
     },[router]) 
-    return data
+    */
+    useEffect(()=>{
+        if(data && !data.ok){
+            router.replace("/enter");
+        }
+
+    }, [data, router])
+
+    return {user: data?.profile, isLoading: !data && !error}
 }
